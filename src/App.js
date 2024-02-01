dc7ef75541d4ecf73eb8832aec82abbb865ca8c0
linivan2003 <linivan2003@gmail.com>
2024-01-31 20:21:43 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -6,17 +6,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Ivan's Portfolio Website</h1>
+        <div>
+          <h2>About Me</h2>
+          <p>
+            Welcome to my portfolio! I am passionate about web development and enjoy working with technologies like React.
+            This website showcases some of my projects and skills. Feel free to explore and learn more about what I do.
+          </p>
+        </div>
       </header>
     </div>
   );
